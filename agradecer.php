fdb84418b7162f14cff77b141cfd00e20b3278ed
AbrahamGN <agarcianevado.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 08:51:06 +0100

    Add files via upload

---
+++ agradecer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Agradece en Compañía</title>
+    <meta name="author" content="Abraham García Nevado">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <nav>
+
+        <div class="logo">
+            <a href="inicio.html">Agradecer en Compañía</a>
+        </div>
+
+        <div class="menu">
+            <a href="#">Agradecer</a>
+            <a href="recibir.html">Recibir</a>
+            <a href="index.html">Cerrar Sesión</a>
+        </div>
+
+    </nav>
+
+
+    <h1>AGRADECE EN COMPAÑÍA</h1>
+
+    <h2>Enviar Agradecimiento</h2>
+
+    <form action="enviar.php" method="post">
+
+        <label for="para">Para:</label><br>
+
+        <select id="para" name="para" required> 
+            <?php
+
+                //Extrae cada una fila del resultado de la consulta
+                while($fila=$resultado->fetch_array()){
+                //Ejemplo que muestra un campo   
+                    echo '<option value=="'.$fila["idAlumno"].'">';
+                    echo $fila["nombre"].'-'.$fila["idAlumno"];
+                    echo '</option>';
+                }
+            
+            ?>
+        </select>
+
+        <br><br>
+
+        <label for="mensaje">Quiero agradecerte:</label><br>
+
+        <textarea id="mensaje" name="mensaje" rows="5" required></textarea>
+
+        <br><br>
+
+        <input type="submit" value="Enviar Agradecimiento">
+
+    </form>
+
+</body>
+</html>
